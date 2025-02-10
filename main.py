138f64b5b0220468caba9e960b0dcdb9a2965883
MeeeeeSia <anastasia2012mariad@gmail.com>
2025-02-10 20:11:51 +0200

    Added a slider and displaying an image.

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
-
+from tkinter import PhotoImage
 from PIL import Image
-image_idk = 'Test.png'
+image_idk = 'C:\\Users\\Lenovo\\Desktop\\Python\\Image-App\\test.png'
 
 image = Image.open(image_idk)
 
 
+def open_prompt():
+   messagebox.showinfo("Message", "Click Okay to Proceed")
+
+
+def update_label(value):
+    label.config(text=f"Value: {value}")
 
 def hello_world():
    print('hello world')
@@ -18,11 +24,17 @@ def b_w():
 
 win = Tk()
 win.geometry("700x350")
+image = PhotoImage(file='C:\\Users\\Lenovo\\Desktop\\Python\\Image-App\\test.png')
 
-
-def open_prompt():
-   messagebox.showinfo("Message", "Click Okay to Proceed")
-
+scale_var = IntVar()
+scale_var.set(50)
+# Create a ttk Scale widget with variable and command options
+scale = ttk.Scale(win, from_=0, to=100, length=300, variable=scale_var, command=update_label)
+scale.pack()
+label =ttk.Label(win, text="Value: 50")
+label.pack()
+image_label = ttk.Label(win, image=image)
+image_label.pack()
 
 Label(win, text= "Click to Open the MessageBox").pack(pady=15)
 
